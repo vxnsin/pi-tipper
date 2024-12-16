3ad1852a5a23e83d1a7860a58d21bcc87ca12a50
Vensin <luiskanzewitsch@gmail.com>
2024-12-16 20:47:01 +0100

    I'm stupid as hell

--- pi.js
+++ pi.js
@@ -1,6 +1,3 @@
-/* 
-© Vensin 2025
-*/
 import readline from 'readline';
 import chalk from 'chalk';
 import figlet from 'figlet';
@@ -8,29 +5,27 @@ import gradient from 'gradient-string';
 
 const PI = Math.PI.toString().substring(2); 
 let currentIndex = 0; 
+let progress = '3.'; 
 
 function showBanner() {
   console.clear();
+
   console.log(gradient(["blue", "pink"])(figlet.textSync('PI Tipper!', { horizontalLayout: 'default' })));
   console.log(chalk.cyan('Type as many digits of PI as you can correctly.'));
   console.log(chalk.yellow('A wrong input will end the game.'));
-  console.log(gradient.pastel('🌟 Made by Vensin 🌟\n'));
+  console.log(gradient.pastel('🌟 Made by Vensin 🌟'));
+  console.log('');
 }
 
 function updateProgress() {
   process.stdout.clearLine(0);
-  process.stdout.cursorTo(0);
-  process.stdout.write(`> ${'3.' + chalk.green(PI.slice(0, currentIndex))}`);
-}
-
-function endGame() {
-  console.log(chalk.red(`\nGame Over! You correctly guessed ${chalk.yellow(currentIndex)} digits of PI.`));
-  process.exit();
+  process.stdout.cursorTo(0); 
+  process.stdout.write(`> ${progress}`);
 }
 
 function setupInput() {
   readline.emitKeypressEvents(process.stdin);
-  process.stdin.setRawMode(true);
+  process.stdin.setRawMode(true); 
 
   process.stdin.on('keypress', (str, key) => {
     if (key.sequence === '\u0003') { 
@@ -43,21 +38,25 @@ function setupInput() {
     const currentDigit = PI[currentIndex];
 
     if (str === currentDigit) {
-      currentIndex++;
+      currentIndex++; 
+      progress += chalk.green(str); 
       updateProgress();
 
       if (currentIndex === PI.length) {
-        console.log(chalk.blue('\nUnbelievable! You guessed all known digits of PI.'));
-        endGame();
+        console.log(chalk.blue('\n\nUnbelievable! You guessed all known digits of PI.'));
+        process.exit();
       }
     } else {
-      console.log(chalk.red(`\n✘ Incorrect! The correct digit was: ${chalk.yellow(currentDigit)}`));
-      endGame();
+      progress += chalk.red(str); 
+      updateProgress();
+      console.log(chalk.red(`\n\n✘ Incorrect! The correct digit was: ${chalk.yellow(currentDigit)}`));
+      process.exit();
     }
   });
 }
 
 process.stdout.write('\x1b]0;PI Tipper by Vensin\x07');
+
 showBanner();
 updateProgress();
 setupInput();
